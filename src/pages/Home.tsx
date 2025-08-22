c842ed1988b6eee817266cfc434f8dce04fc0df2
curtiskelton88 <146534479+curtiskelton88@users.noreply.github.com>
2025-08-22 15:48:37 +0200

    Remove static testimonial and keep sliding carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -304,6 +304,9 @@ function Home() {
         </div>
       </section>
 
+      {/* Client Logos - Moved to after Ready to Start Building Authority */}
+      <ClientLogos />
+
       {/* Testimonials Section - Sliding Carousel */}
       <section className="py-20 bg-gradient-to-br from-gray-50 to-blue-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -398,9 +401,6 @@ function Home() {
           </div>
         </div>
       </section>
-
-      {/* Client Logos - Moved to after Ready to Start Building Authority */}
-      <ClientLogos />
     </>
   );
 }
